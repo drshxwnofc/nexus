8084c6270a0dc52fd1d3f10a1a4686a4d8d4940d
Dr SHXWN <seanmandivhei47@gmail.com>
2026-01-05 21:19:38 +0200

    Create freeze.js

---
+++ api/admin/freeze.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+import kv from "../db/kv.js";
+
+export default async function handler(req, res) {
+  try {
+    const admin = jwt.verify(req.cookies.admin, process.env.JWT_SECRET);
+    if (admin.role !== "admin") throw new Error();
+
+    const { id } = req.body;
+    await kv.set(`freeze:${id}`, "ADMIN");
+
+    res.json({ success: true });
+  } catch {
+    res.status(403).end();
+  }
+}
